804de4055e6e47743bdfbc8311bc5598b264b29d
leotutoring <leo@tutoring.co.kr>
2018-12-24 17:57:33 +0900

    UPDATE server static files

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Credentials", true);
 	next();
 });
+app.use(express.static(path.join(__dirname, 'client/build')))
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
