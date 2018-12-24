69a0419b8a63ce98ac93df8ca69955d6ca2ec51a
leotutoring <leo@tutoring.co.kr>
2018-12-24 17:36:56 +0900

    UPDATE can deploy

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
 const express = require("express"),
-  http = require("http"),
-  bodyParser = require("body-parser"),
-  morgan = require("morgan");
+	http = require("http"),
+	bodyParser = require("body-parser"),
+	morgan = require("morgan"),
+	path = require("path");
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.use(morgan("dev"));
 app.use(
-  bodyParser.urlencoded({
-    limit: "50mb",
-    extended: true,
-    parameterLimit: 1000000
-  })
+	bodyParser.urlencoded({
+		limit: "50mb",
+		extended: true,
+		parameterLimit: 1000000
+	})
 );
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, content-type, Authorization, Content-Type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, OPTIONS, PUT, PATCH, DELETE"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"X-Requested-With, content-type, Authorization, Content-Type"
+	);
+	res.setHeader("Access-Control-Allow-Credentials", true);
+	next();
 });
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname+'/client/build/index.html'));
+	res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 app.use("/api", require("./routes/index"));
 http.createServer(app).listen(PORT, () => {
-  console.log(`SERVER MESSAGE : HTTP Server running on PORT: ${PORT}`);
+	console.log(`SERVER MESSAGE : HTTP Server running on PORT: ${PORT}`);
 });
