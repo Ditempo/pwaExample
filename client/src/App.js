5f7bf684c3a74404100eeed75c798cb5246131a6
leotutoring <leo@tutoring.co.kr>
2018-12-24 18:03:27 +0900

    UPDATE connect server to front

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {data: ""};
+
+	async componentDidMount() {
+		const data = await fetch("/api/course");
+		this.setState({data: data});
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<h1>{this.state.data}</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
